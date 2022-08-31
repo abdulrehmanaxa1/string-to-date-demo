70234aafcdab2f27c618711dfb458c7f3bf01269
abdulrehmanaxa1 <abdul.rehman1@axabiztech.com>
2022-08-31 17:37:03 +0500

    Updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {StringToDateService} from "string-to-date-convert";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'string-to-date';
+
+  form: FormGroup;
+
+  format1 = '22042021';
+  format2 = '22/11/2022';
+  format3 = '12/31/2022';
+  format4 = '2009-01-31';
+
+  constructor(private fb: FormBuilder,
+              private stringToDate: StringToDateService){
+    this.createForm();
+  }
+
+  createForm(){
+    this.form = this.fb.group({
+      formate: [null]
+    })
+  }
+
+  testingDates(date, format){
+    debugger
+    this.form.controls['formate'].setValue(this.stringToDate.stringToDate(date, format));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
